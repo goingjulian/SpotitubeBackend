c9dfa4805f74e47ef8ea442e8a29637b6360d9fe
Julian Korf de Gidts <julian@korfdegidts.nl>
2018-10-14 15:27:21 +0200

    Added additional SQL script

---
+++ src/main/database/spotitube_trackPlaylist.sql
@@ -0,0 +1,24 @@
+INSERT INTO spotitube.trackPlaylist (track_id, playlist_id, offlineAvailable)
+VALUES (8, 18, 1);
+INSERT INTO spotitube.trackPlaylist (track_id, playlist_id, offlineAvailable)
+VALUES (19, 8, 1);
+INSERT INTO spotitube.trackPlaylist (track_id, playlist_id, offlineAvailable)
+VALUES (16, 8, 1);
+INSERT INTO spotitube.trackPlaylist (track_id, playlist_id, offlineAvailable)
+VALUES (22, 7, 1);
+INSERT INTO spotitube.trackPlaylist (track_id, playlist_id, offlineAvailable)
+VALUES (17, 7, 1);
+INSERT INTO spotitube.trackPlaylist (track_id, playlist_id, offlineAvailable)
+VALUES (17, 8, 0);
+INSERT INTO spotitube.trackPlaylist (track_id, playlist_id, offlineAvailable)
+VALUES (18, 8, 0);
+INSERT INTO spotitube.trackPlaylist (track_id, playlist_id, offlineAvailable)
+VALUES (23, 25, 1);
+INSERT INTO spotitube.trackPlaylist (track_id, playlist_id, offlineAvailable)
+VALUES (24, 25, 0);
+INSERT INTO spotitube.trackPlaylist (track_id, playlist_id, offlineAvailable)
+VALUES (20, 25, 0);
+INSERT INTO spotitube.trackPlaylist (track_id, playlist_id, offlineAvailable)
+VALUES (25, 26, 0);
+INSERT INTO spotitube.trackPlaylist (track_id, playlist_id, offlineAvailable)
+VALUES (25, 7, 0);
